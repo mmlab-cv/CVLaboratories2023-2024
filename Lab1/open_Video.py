cc02f574a2023aaaa36c051d3b990b3d465c3a77
giuli13 <62147733+giuli13@users.noreply.github.com>
2023-03-27 16:36:06 +0200

    Update open_Video.py

--- Lab1/open_Video.py
+++ Lab1/open_Video.py
@@ -3,7 +3,7 @@ import cv2
 
 ## CAPTURE VIDEO FROM CAMERA
 
-cap = cv2.VideoCapture('Video.mp4')   #cap = cv2.VideoCapture('open_Video.py')
+cap = cv2.VideoCapture(0)   #cap = cv2.VideoCapture('Video.mp4')
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
