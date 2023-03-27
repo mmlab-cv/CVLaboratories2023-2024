818ebe0841d8396d219c1a92cd5e0a286655499b
giuli13 <62147733+giuli13@users.noreply.github.com>
2023-03-27 16:35:37 +0200

    Update open_Video.py

--- Lab1/open_Video.py
+++ Lab1/open_Video.py
@@ -3,33 +3,30 @@ import cv2
 
 ## CAPTURE VIDEO FROM CAMERA
 
-# cap = cv2.VideoCapture('Video.mp4')   #cap = cv2.VideoCapture('open_Video.py')
+cap = cv2.VideoCapture('Video.mp4')   #cap = cv2.VideoCapture('open_Video.py')
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
-# if not cap.isOpened():
-#     print("Cannot open camera")
-#     exit()
+while True:
+# Capture frame-by-frame
+     ret, frame = cap.read()
+     # if frame is read correctly ret is True
+     if not ret:
+         print("Can't receive frame (stream end?). Exiting ...")
+         break
+     # # Our operations on the frame come here
+     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+     # Display the resulting frame
+     cv2.imshow('frame', gray)
+     if cv2.waitKey(1) == ord('q'):
+         break
 
-# while True:
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-#     # if frame is read correctly ret is True
-#     if not ret:
-#         print("Can't receive frame (stream end?). Exiting ...")
-#         break
-#     # # Our operations on the frame come here
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     # Display the resulting frame
-#     cv2.imshow('frame', gray)
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-
-# # When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
+ # When everything done, release the capture
+ cap.release()
+ cv2.destroyAllWindows()
 
 ## SAVE VIDEO
-import numpy as np
-import cv2 as cv
 cap = cv.VideoCapture(0)
 # Define the codec and create VideoWriter object
 fourcc = cv.VideoWriter_fourcc(*'XVID')
